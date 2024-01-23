d53d9fcc4bad9270ad8298f222e39272552ee18f
Thushar Sreenivas <thushar@keyvalue.systems>
2024-01-23 15:25:09 +0530

    Fix build failure on startup

--- packages/react-sdk/src/App.js
+++ packages/react-sdk/src/App.js
@@ -1,15 +1,24 @@
-// packages/react-sdk/src/App.js
-import React from 'react';
-import AddComponent from './AddComponent';
+import { sharedVariable } from '@siren-sdk/core';
 
 function App() {
-    return (
-        <div>
-            <header>
-                <AddComponent />
-            </header>
-        </div>
-    );
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div>{sharedVariable}</div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
 }
 
 export default App;
